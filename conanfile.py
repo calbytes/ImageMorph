c12631d0bdcb2e853240c1a8b9f4e282d4bfd53c
calbytes <caalla3@gmail.com>
2024-04-04 00:17:16 -0400

    test

--- conanfile.py
+++ conanfile.py
@@ -2,11 +2,16 @@ from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
 class ImageMorphRecipe(ConanFile):
+    name = "ImageMorph"
+    version = "1.0.0"
+    license = "CaliRig LLC"
+    author = "CAL"
+    description = "Transforms Image and Video files"
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-        self.requires("gtest/1.8.1")
+        self.requires("gtest/1.14.0")
 
     def layout(self):
         cmake_layout(self)
--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <gtest/gtest.h>
 
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
-    
-    return 0;
+int main(int argc, char **argv) {
+  if (argc != 7) {
+    std::cout <<"Not 7" << std::endl;
+    return 1;
+  }
+  else{
+    std::cout <<"Hello World!" << std::endl;
+  }
 }
